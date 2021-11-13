56b36f19930cab619fed4fe0b71314e832afccec
peterstastny <peter.stastny7@gmail.com>
2021-11-13 23:48:03 +0100

    landing animation

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const landing = {
-  duration: 1000,
-  iterations: 1,
-  easing: "ease-out",
-};
+"use strict";
 
-const keayframes = [
-  { transform: "translateX(100px)", opacity: 0 },
-  { transform: "translateX(0)", opacity: 1 },
-];
+window.addEventListener("DOMContentLoaded", animateLanding);
 
-const photo = document.querySelector(".portrait");
-const animation = photo.animate(keayframes, landing);
+function animateLanding() {
+  const landing = {
+    duration: 1000,
+    iterations: 1,
+    easing: "ease-out",
+  };
+
+  const keayframesPhoto = [
+    { transform: "translateX(100px)", opacity: 0 },
+    { transform: "translateX(0)", opacity: 1 },
+  ];
+
+  const photo = document.querySelector(".portrait");
+
+  const animationPhoto = photo.animate(keayframesPhoto, landing);
+
+  const keayframesHeadingName = [
+    { transform: "translateX(-100px)", opacity: 0 },
+    { transform: "translateX(0)", opacity: 1 },
+  ];
+
+  const headingName = document.querySelector(".name");
+  const animationHeadingName = headingName.animate(keayframesHeadingName, landing);
+
+  const keayframesHeadingDescription = [
+    { transform: "translateX(-100px)", opacity: 0 },
+    { transform: "translateX(0)", opacity: 1 },
+  ];
+
+  const headingDesc = document.querySelector(".description");
+
+  const animationHeadingDesc = headingDesc.animate(keayframesHeadingDescription, landing);
+}
